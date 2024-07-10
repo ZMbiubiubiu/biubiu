100c7c83b1c86b112efed4da7e50311ef48c8f18
上杉夏香 <390047718@qq.com>
2024-07-11 00:32:03 +0800

    finish trie tree

--- biubiu/trie.go
+++ biubiu/trie.go
@@ -4,19 +4,35 @@ package biubiu
 
 import "strings"
 
+type TrieTree struct {
+	root *node
+}
+
+func NewTrie() *TrieTree {
+	return &TrieTree{root: &node{}}
+}
+
+func (t *TrieTree) Search(fullPath string) interface{} {
+	return t.root.search(fullPath)
+}
+
+func (t *TrieTree) Insert(fullPath string, value interface{}) {
+	t.root.insert(fullPath, value)
+}
+
+func (t *TrieTree) Clear() {
+	t.root = &node{}
+}
+
 type node struct {
 	fullPath string
 	partPath string
 	isWild   bool // 是否模糊匹配：*/:var
-	handler  interface{}
+	value    interface{}
 	children []*node
 }
 
-func NewNode() *node {
-	return &node{}
-}
-
-func (n *node) parsePath(fullPath string) []string {
+func parsePath(fullPath string) []string {
 	splitPath := strings.Split(fullPath, "/")
 
 	var parts = make([]string, 0, len(splitPath))
@@ -30,4 +46,60 @@ func (n *node) parsePath(fullPath string) []string {
 		}
 	}
 	return parts
+}
+
+func (n *node) matchFirstChild(part string) *node {
+	for _, child := range n.children {
+		if child.isWild || child.partPath == part {
+			return child
+		}
+	}
+	return nil
+}
+func (n *node) matchChildren(part string) []*node {
+	var children = make([]*node, 0, len(n.children))
+	for _, child := range n.children {
+		if child.isWild || child.partPath == part {
+			children = append(children, child)
+		}
+	}
+	return children
+}
+
+func (n *node) insert(fullPath string, value interface{}) {
+	parts := parsePath(fullPath)
+	for _, part := range parts {
+		child := n.matchFirstChild(part)
+		if child == nil {
+			child = &node{
+				partPath: part,
+				isWild:   part[0] == '*' || part[0] == ':',
+			}
+			n.children = append(n.children, child)
+		}
+		n = child
+	}
+	// for the last node
+	n.fullPath = fullPath
+	n.value = value
+}
+
+func (n *node) search(fullPath string) interface{} {
+	parts := parsePath(fullPath)
+	var children = []*node{n}
+	for _, part := range parts {
+		children = n.doSearch(part, children)
+	}
+	if len(children) == 0 {
+		return nil
+	}
+	return children[0].value
+}
+
+func (n *node) doSearch(part string, nodes []*node) []*node {
+	var matched []*node
+	for _, node := range nodes {
+		matched = append(matched, node.matchChildren(part)...)
+	}
+	return matched
 }
--- biubiu/trie_test.go
+++ biubiu/trie_test.go
@@ -33,13 +33,39 @@ func TestParsePath(t *testing.T) {
 		},
 	}
 
-	node := NewNode()
-
 	for _, table := range tables {
-		got := node.parsePath(table.input)
+		got := parsePath(table.input)
 		assert.Equal(t, len(table.output), len(got))
 		for i, part := range got {
 			assert.Equal(t, table.output[i], part)
 		}
+	}
+}
+
+func TestInsertAndSearch(t *testing.T) {
+	var tables = []struct {
+		fullPath string
+		value    int
+	}{
+		{
+			fullPath: "/hello/world",
+			value:    2,
+		},
+		{
+			fullPath: "/hello/",
+			value:    1,
+		},
+	}
+
+	trie := NewTrie()
+
+	for _, table := range tables {
+		trie.Insert(table.fullPath, table.value)
+		got := trie.Search(table.fullPath)
+		assert.NotNil(t, got)
+		assert.Equal(t, table.value, got.(int))
+		trie.Clear()
+		got = trie.Search(table.fullPath)
+		assert.Nil(t, got)
 	}
 }
